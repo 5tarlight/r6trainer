e14e35b805e46c076ab56064b58d90108aee8e7f
Kim Kyu San <developerstarlight@gmail.com>
2020-01-03 01:23:47 +0900

    moving box

---
+++ r6/r6.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "r6.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, r6, "r6" );
---
+++ r6/r6GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "r6GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class R6_API Ar6GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
